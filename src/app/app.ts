33ee5704c9ad2ad0a64278b70714a39dd2fb4197
risercable <jerald.mark.buljatin@gmail.com>
2026-02-28 18:39:56 +0800

    Somewhere around building initial Header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   title = 'Skinet';
