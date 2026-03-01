d84c8d258c21864f06772e0359f33149bf635961
jmbul <jerald.mark.buljatin@gmail.com>
2026-03-01 17:15:23 +0800

    Added basic Api call

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import {HttpClient} from '@angular/common/http';
+import {Product} from './shared/models/product';
+import {Pagination} from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,17 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  baseUrl: string = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log(true),
+    })
+  }
 }
---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  pictureUrl: string;
+  price: number;
+  quantityInStc: number;
+  type: string;
+}
