38a548c5d0f022ef5a968f29a3d8790b76914e3e
jmbul <jerald.mark.buljatin@gmail.com>
2026-03-02 17:10:07 +0800

    Removed provider for Zone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,13 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import 'zone.js'
 import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZoneChangeDetection(),
     provideHttpClient()
   ]
 };
